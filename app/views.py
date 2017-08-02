1ae93a97caddf066d09941a784588200c5619bac
Sam Naser <sammnaser@gmail.com>
2017-08-02 12:18:42 -0400

    Load articles from csv from load-articles endpoint

--- app/views.py
+++ app/views.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from rest_framework import viewsets, permissions 
 from app.serializers import ArticleSerializer, UserSerializer, ClickthroughSerializer
 from app.models import Article, Clickthrough
 
+import os 
+import csv
 
 class ArticleViewSet(viewsets.ModelViewSet): 
 	queryset = Article.objects.all() 
@@ -23,3 +26,26 @@ class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all() 
 	serializer_class = UserSerializer
 	model = User
+
+#route that will load our articles from csv file
+def loadArticles(request):
+	BASE = os.path.dirname(os.path.abspath(__file__))
+	ARTICLE_CSV_PATH = os.path.join(BASE, 'articles.csv')
+	with open(ARTICLE_CSV_PATH, 'r') as articleFile: 
+		reader = csv.reader(articleFile)
+		for article in reader: 
+
+			print article
+    		
+			if article[0] == 'Title': 
+				continue
+
+			_, a = Article.objects.get_or_create( 
+				title= article[0], 
+				description= article[1],
+				imageUrl= article[2],
+				link= article[3],
+				bias= article[4]	
+			)
+
+	return HttpResponse(status= 201)
